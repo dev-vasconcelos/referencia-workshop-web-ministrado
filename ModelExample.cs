7919fd0c7ab8f0dcbb5ec8e35789c21f014aa369
dev-vasconcelos <pedroivomartinsdevasconcelos@gmail.com>
2021-09-29 16:24:37 -0300

    Update ModelExample.cs

--- ModelExample.cs
+++ ModelExample.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiExemplo.Models {
    
-  [Table("tb_usuario")]
+  [Table("tb_usuario")] // annotation que define o da classe no banco de dados
   public class Usuario {
-    [Key]
-    [Column("id")]
+    
+    [Key] // annotation que define esta variável como PK(Primary Key)
+    [Column("id")]// annotation que define o nome desta variável no banco de dados
     public long Id {get; set;}
 
     [Column("name")]
